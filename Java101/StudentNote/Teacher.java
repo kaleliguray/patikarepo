d1aec5bcdbd5ccbd970b5ca5a0ade9c32f87e3a4
“guraykaleli” <kaleligiray@gmail.com>
2021-08-16 14:55:27 +0300

    student

---
+++ Java101/StudentNote/Teacher.java
@@ -0,0 +1,18 @@
+package com.studentMarkAutomation;
+
+public class Teacher {
+
+    String name;
+    String mobilePhoneNumber;
+    String branch;
+
+    public Teacher(String name, String mobilePhoneNumber, String branch) {
+        this.name = name;
+        this.mobilePhoneNumber = mobilePhoneNumber;
+        this.branch = branch;
+    }
+
+
+
+
+}
